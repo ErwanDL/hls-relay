b69b7cef95cc206f3c92838e687eb43b3356eb59
Erwan <erwan.delepinau@free.fr>
2020-12-01 11:45:24 +0100

    implement and test basic interceptor

---
+++ hlsrelay/interceptor.py
@@ -0,0 +1,32 @@
+import sys
+import time
+from typing import Awaitable, Callable, TextIO
+from urllib.parse import urljoin
+
+from aiohttp import web
+
+
+class StreamInterceptor:
+    def __init__(
+        self,
+        stream_url: str,
+        request_executor: Callable[[str], Awaitable[web.Response]],
+        output: TextIO = sys.stdout,
+    ) -> None:
+        self._stream_url = stream_url
+        self._request_executor = request_executor
+        self._output = output
+
+    async def intercept(self, request: web.Request) -> web.Response:
+        path_to_resource = request.match_info.get("path_to_resource", "")
+        full_path = urljoin(self._stream_url, path_to_resource)
+        self._log(f"[IN] {full_path}")
+        t1 = time.time()
+        stream_res = await self._request_executor(full_path)
+        t2 = time.time()
+        time_elapsed_ms = round(t2 - t1, 6) * 1000
+        self._log(f"[OUT] {full_path} ({time_elapsed_ms}ms)")
+        return stream_res
+
+    def _log(self, text: str) -> None:
+        print(text, file=self._output)
---
+++ test/unit/test_interceptor.py
@@ -0,0 +1,59 @@
+from io import StringIO
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import MagicMock
+
+from aiohttp import web
+from hlsrelay.interceptor import StreamInterceptor
+
+test_headers = {
+    "Accept-Ranges": "bytes",
+    "Content-Type": "audio/x-mpegurl",
+    "Etag": '"a852221858992283a46628accca46f33:1509454736"',
+    "Last-Modified": "Tue, 31 Oct 2017 12:58:56 GMT",
+    "Server": "TestServer",
+    "Content-Length": "9",
+    "Expires": "Tue, 01 Dec 2020 10:00:34 GMT",
+    "Cache-Control": "max-age=0, no-cache, no-store",
+    "Pragma": "no-cache",
+    "Date": "Tue, 01 Dec 2020 10:00:34 GMT",
+    "Connection": "keep-alive",
+    "Access-Control-Max-Age": "86400",
+    "Access-Control-Allow-Credentials": "false",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Methods": "GET,POST,HEAD",
+    "Access-Control-Allow-Origin": "*",
+}
+
+
+async def fake_request_executor(url: str) -> web.Response:
+    return web.Response(
+        status=206,
+        body=bytes(f"A test request was sent to {url}", encoding="utf-8"),
+        headers=test_headers,
+    )
+
+
+class TestStreamInterceptor(IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        self.output = StringIO()
+        self.interceptor = StreamInterceptor(
+            "http://test.com", request_executor=fake_request_executor, output=self.output
+        )
+
+    async def test_relays_correct_response(self) -> None:
+        mock_request = MagicMock()
+        mock_request.match_info = {"path_to_resource": "playlist.m3u8"}
+        res = await self.interceptor.intercept(mock_request)
+        self.assertEqual(res.status, 206)
+        self.assertEqual(res.headers, test_headers)
+        self.assertEqual(res.body, b"A test request was sent to http://test.com/playlist.m3u8")
+
+    async def test_logs_incoming_request_and_response(self) -> None:
+        mock_request = MagicMock()
+        mock_request.match_info = {"path_to_resource": "video.ts"}
+        _ = await self.interceptor.intercept(mock_request)
+
+        logs = self.output.getvalue().splitlines()
+        self.assertEqual(len(logs), 2)
+        self.assertEqual(logs[0], "[IN] http://test.com/video.ts")
+        self.assertRegex(logs[1], r"\[OUT\] http://test.com/video.ts \([0-9]+\.[0-9]+ms\)")
