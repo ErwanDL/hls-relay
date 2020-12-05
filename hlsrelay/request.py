14ae820cb8fe23d760e8599088e1fd629511e754
Erwan <erwan.delepinau@free.fr>
2020-12-05 17:04:48 +0100

    use a persistent ClientSession

--- hlsrelay/request.py
+++ hlsrelay/request.py
@@ -1,8 +1,11 @@
-from aiohttp import ClientSession, web
+from aiohttp import web
 
 
-async def get_request(url: str) -> web.Response:
-    async with ClientSession() as session:
-        async with session.get(url) as res:
+class RequestClient:
+    def __init__(self, app: web.Application) -> None:
+        self._app = app
+
+    async def get_request(self, url: str) -> web.Response:
+        async with self._app["session"].get(url) as res:
             body = await res.read()
             return web.Response(status=res.status, headers=res.headers, body=body)
--- hlsrelay/server.py
+++ hlsrelay/server.py
@@ -2,13 +2,22 @@ import sys
 from typing import TextIO
 
 from aiohttp import web
+from aiohttp.client import ClientSession
 
 from hlsrelay.interceptor import StreamInterceptor
-from hlsrelay.request import get_request
+from hlsrelay.request import RequestClient
+
+
+async def persistent_session(app: web.Application):
+    app["session"] = ClientSession()
+    yield
+    await app["session"].close()
 
 
 def create_app(base_url: str, out: TextIO = sys.stdout) -> web.Application:
     app = web.Application()
-    interceptor = StreamInterceptor(base_url, request_executor=get_request, output=out)
+    client = RequestClient(app)
+    interceptor = StreamInterceptor(base_url, request_executor=client.get_request, output=out)
     app.add_routes([web.get("/{resource_URI:.*}", interceptor.intercept)])
+    app.cleanup_ctx.append(persistent_session)
     return app
